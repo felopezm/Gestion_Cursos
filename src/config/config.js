268c12ad4d038a21fa966c421e334ccbe7f25e88
felopezm <fedelopezm@gmail.com>
2019-06-30 20:59:04 -0500

    config

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,2 @@
 process.env.PORT = process.env.PORT || 3000;
-process.env.URLDB = 'mongodb+srv://fede:<fede>@cluster0-2wst6.mongodb.net/gestion_cursos?retryWrites=true&w=majority';
-//process.env.URLDB = 'mongodb://localhost:27017/gestion_cursos';
+process.env.URLDB = 'mongodb://localhost:27017/gestion_cursos';
